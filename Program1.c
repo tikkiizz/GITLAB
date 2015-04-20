2f73f945123730d44e39b601a4b728e9df88428f
tikkiizz <tikkiizz@hotmail.com>
2015-04-20 20:02:41 +0700

    Add new 1DOC and 2C File

---
+++ Program1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("Enter your number: ");
+    scanf("%d",&x);
+    printf("Hello Your number is %d\n",x);
+    return 0;
+}
---
+++ Program2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[10];
+    printf("Enter your name: ");
+    scanf("%s",name);
+    printf("Hello Your Name is %s\n",name);
+    return 0;
+}
